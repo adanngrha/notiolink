8544c3a78b07c27e859f56c72e1b5c2a3cea0050
Adan Nugraha <contact.ardana@gmail.com>
2022-02-15 15:52:19 +0700

    Update notiolink.config.js

--- notiolink.config.js
+++ notiolink.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   /** Application Name, will be shown on the index page */
-  appName: 'Adan Nugraha',
+  appName: 'Ardana Nugraha',
 
   /** Description for SEO */
   seoDescription:
